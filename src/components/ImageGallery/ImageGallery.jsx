bcccdb00a24e84fd40fe496520a2151a573b1b78
Kulik Victor <kulic02041988@gmail.com>
2023-01-10 21:36:59 +0200

    finished HW-3/2

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,45 @@
+import { Component } from 'react';
+import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
+import { Loader } from 'components/Loader/Loader';
+import { Button } from 'components/Button/Button';
+import PropTypes from 'prop-types';
+
+export class ImageGallery extends Component {
+  render() {
+    const { status, imageList, loadMore } = this.props;
+
+    if (status === 'resolved' || status === 'pending') {
+      return (
+        <>
+          <ul className="ImageGallery">
+            {imageList.length > 0 &&
+              imageList.map(image => {
+                const { id, webformatURL, largeImageURL, user } = image;
+
+                return (
+                  <ImageGalleryItem
+                    key={id}
+                    webformatURL={webformatURL}
+                    largeImageURL={largeImageURL}
+                    alt={user}
+                  />
+                );
+              })}
+          </ul>
+
+          {status === 'pending' ? <Loader /> : <Button loadMore={loadMore} />}
+        </>
+      );
+    }
+  }
+}
+
+ImageGalleryItem.propTypes = {
+  webformatURL: PropTypes.string.isRequired,
+  largeImageURL: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+};
+
+Button.propTypes = {
+  loadMore: PropTypes.func.isRequired,
+};
